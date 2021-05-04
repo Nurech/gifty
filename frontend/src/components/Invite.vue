6df3c94766927d2bd7852259196ce3c62aad7d43
MerlynValma <merlyn.valma@gmail.com>
2021-05-04 16:39:43 +0300

    invite tabel_taastamine

--- frontend/src/components/Invite.vue
+++ frontend/src/components/Invite.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="invite">
-
+    <h3>Add your participants</h3>
+    <br>
     <table border="1">
       <thead>
+      <th>Guest id</th>
       <th>Guest name</th>
       <th>Role</th>
       <th>E-mail</th>
@@ -11,9 +13,10 @@
       </thead>
       <tbody>
       <tr v-for="(item, index) in tableRows" :item="item">
-        <td>{{ item.guestName }}</td>
+        <td>{{ item.guestId }}</td>
+        <td><input v-model="item.guestName" placeholder="Guest name"/></td>
         <td>{{ item.role }}</td>
-        <td>{{ item.email }}</td>
+        <td><input v-model="item.email" placeholder="E-mail"/></td>
         <td>{{ item.generateLink }}</td>
         <td>
           <button @click='delTableRow(index)'>Remove</button>
@@ -28,22 +31,37 @@
   </div>
 </template>
 <script type="text/javascript">
-
-
 export default {
   data: function () {
     return {
-      'guestName': "Guest Name",
-      'role': "Owner",
-      'email': "owner@owner.ee",
-      'generateLink': "some URL",
+      'guestId': "",
+      'guestName': "",
+      'role': "",
+      'email': "",
+      'generateLink': "",
       'deleteButton': "",
-      'tableRows': []
+      'randomNumber': "",
+      'tableRows': [{}]
     }
   },
   methods: {
+    "dbGuestId": function () {
+      post('/api/user/', {
+        guestId: this.guestId
+      })
+          .then((response) => {
+            this.guestId = response.data
+          })
+    },
     addTableRow: function () {
+      this.guestId = getRandom(15);
+      this.guestName = "";
+      this.role = "user";
+      this.email = "";
+      this.generateLink = "";
+      this.deleteButton = "";
       let my_objects = {
+        guestId: this.guestId,
         guestName: this.guestName,
         role: this.role,
         email: this.email,
@@ -51,18 +69,20 @@ export default {
         deleteButton: this.deleteButton
       };
       this.tableRows.push(my_objects);
-      this.guestName = "Guest " + this.tableRows.length;
-      this.role = "";
-      this.email = "";
-      this.generateLink = "";
-      this.deleteButton = "";
     },
     delTableRow: function (id) {
-         this.tableRows.splice(id, 1);
+      this.tableRows.splice(id, 1);
     }
   }
 };
-
+function getRandom(length) {
+  var random = '0123456789';
+  var result = "";
+  for (var i = 0; i < length; i++) {
+    result += random.charAt(Math.floor(Math.random() * random.length));
+  }
+  return result;
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -70,17 +90,14 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
 }
