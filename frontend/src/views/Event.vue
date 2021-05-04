34aca931a710040e349694fafe9ce6936da5e942
Toomas Prooso <prooso@gmail.com>
2021-05-04 13:26:49 +0300

    04.05.2021 event

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="event">
     <h1>This is an Event page</h1>
+<EventBody/>
+
+
     <Invite/>
+
   </div>
 </template>
 <script>
+
 import Invite from "@/components/Invite";
+import EventBody from "@/components/EventBody";
+
 export default {
   components: {
-    Invite
+    Invite,
+    EventBody
   }
 }
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,19 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld/>
-    <Invite/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import Invite from "@/components/Invite";
+
 
 export default {
   name: 'Home',
   components: {
-    Invite,
     HelloWorld
   }
 }
