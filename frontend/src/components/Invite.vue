65287e7e6d2b9d6f42dbcdd3e7dd11ecd451b163
MerlynValma <merlyn.valma@gmail.com>
2021-05-05 14:29:45 +0300

    05.05 hommik

--- frontend/src/components/Invite.vue
+++ frontend/src/components/Invite.vue
@@ -52,7 +52,7 @@ export default {
   methods: {
     'testButton': function () {
       post('/api/user/', this.tableRows
-        ),
+        )
       this.infoks = this.tableRows;
     },
     addTableRow: function () {
