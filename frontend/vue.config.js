6feb9584c70baf11b99e74b79ca9e383b7390fa5
Joosep <joosep.parts@gmail.com>
2021-05-14 09:00:31 +0300

    router done

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,8 +9,14 @@ module.exports = {
       }
     }
   },
+
   // Change build paths to make them Maven compatible
   // see https://cli.vuejs.org/config/
   outputDir: 'target/dist',
-  assetsDir: 'static'
+
+  assetsDir: 'static',
+  css: {
+    modules: true,
+    sourceMap: true
+  }
 };
